b4abdd5a149af2bd042e23787bd9e151ded76398
Ricardo Ayala <mr.popcorner@hotmail.com>
2016-10-09 22:34:18 -0700

    added main switch

--- main.cpp
+++ main.cpp
@@ -10,21 +10,43 @@
 
 int lineCount = 0;
 int colCount = 0;
+
+std::ifstream ifs;
 int main(int argc, char *argv[])
 {
 	std::string line;
-	std::ifstream ifs;
 	if(argc > 1)
 		ifs.open(argv[1]);
 	else
 		ifs.open("CK.soy");
 	if(ifs.is_open())
 	{
-		while(getline(ifs, line))
-		{
-			std::cout << line << std::endl; 
-		}
+		if(mainSwitch())
+			return 1;
 		ifs.close();
 	}
 	return 0;
 }  //int main(int argc, char *argv[])
+
+int mainSwitch()
+{
+	int scont; //string counter
+	while(getline(ifs, line))
+	{
+		scont = 0;
+		while(scont < line.length())
+		{
+			switch(line[scont])
+			{
+				case ' ': scont++;
+					break;
+				case '\n': scont++;
+					break;
+			}
+			if(scont < 0)
+				return -1;
+			scont++;
+		}
+	}
+	return 0;
+}  //void mainSwitch(string line)
